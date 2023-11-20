b1f70dcc88f647339988c704b3b5ca21809218f7
Regis <regis@air-de-regis.lan>
2023-11-20 16:30:32 +0100

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.box1}>
+        <Image
+          source={{
+            uri: "https://res.cloudinary.com/lereacteur-apollo/image/upload/v1684153265/apollo/lereacteur/React%20Native/logo-imdb_tj4n8g.png",
+          }}
+          style={styles.logo}
+        />
+      </View>
+      <View>
+        <Text style={styles.movieText}>Interstellar </Text>
+        <Text style={styles.descMovie}>
+          2014 PG-13 2h49min Adventure, Drama, Science-Fiction
+        </Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // flex: 1,
+    // backgroundColor: "black",
+
+    box1: {
+      backgroundColor: "grey",
+      height: 50,
+      width: "100%",
+    },
+
+    logo: {
+      width: 150,
+      height: 30,
+      resizeMode: "contain",
+    },
+    movieText: {
+      fontSize: 20,
+      color: white,
+    },
+    descMovie: {
+      fontSize: 10,
+      color: white,
+    },
   },
 });
